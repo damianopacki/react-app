1545b58add4b8af1025bad2f53364839b5df9535
Damian Opacki <d.opacki@vectra.pl>
2018-07-12 10:15:06 +0200

    add ingredient handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    meat: 1.3,
+    cheese: 0.4,
+    bacon: 0.8
+};
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -10,14 +17,28 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        this.setState((prevState, props) => {
+            const updatedIngredients = {...prevState.ingredients}
+            updatedIngredients[type] += 1
+            return (
+                {
+                    ingredients: updatedIngredients,
+                    totalPrice: prevState.totalPrice + INGREDIENT_PRICES[type]
+                }
+            )
+        });
     }
 
     render () {
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler}/>
             </Fragment>
         );
     }
