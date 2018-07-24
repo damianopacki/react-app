c80b5571c0ff50caf4bf4e9d4a6f6b66da34b453
Damian Opacki <d.opacki@vectra.pl>
2018-07-24 11:36:43 +0200

    menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Fragment>
-        <div>Toolbar, SiderDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Fragment>
-)
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    drawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render () {
+        return (
+            <Fragment>
+                <Toolbar clicked={this.drawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Fragment>
+        );
+    }
+}
+
+export default Layout;
