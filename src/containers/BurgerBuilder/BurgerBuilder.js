6064e25a8374863286d80f2ebdb015dca2b7f445
Damian Opacki <d.opacki@vectra.pl>
2018-09-20 12:30:11 +0200

    http request error handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders'
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../components/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -64,7 +65,7 @@ class BurgerBuilder extends Component {
                 deliveryMethod: 'fastest'
             }
         }
-        axios.post('/orders.json', order)
+        axios.post('/orders', order)
             .then(response => this.setState({loading: false, purchasing: false}))
             .catch(error => this.setState({loading: false, purchasing: false}));
     }
@@ -136,4 +137,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
